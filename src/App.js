6e2731f0e5784b825d934199877884ce249592b2
KarSE62 <59428783+KarSE62@users.noreply.github.com>
2021-08-23 21:56:30 +0700

    save

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-      <h4>Just Tattoo</h4>
+        <img className="app-header-logo" src="/img/logo.png" />
       </header>
       <div className="app-grid">
-        <div>loremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</div>
-        <div>loremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</div>
-        <div>loremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</div>
-        <div>loremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</div>
+        <div className="tattoo-item">
+          <img src="/img/tattoo-01-small.jpg" />
+            <h4>ลายไฟ</h4>
+        </div>
+        <div className="tattoo-item">
+          <img src="/img/tattoo-02-small.jpg" />
+            <h4>ลายไฟ</h4>
+        </div>
+        <div className="tattoo-item">
+          <img src="/img/tattoo-03-small.jpg" />
+            <h4>ลายไฟ</h4>
+        </div>
+        <div className="tattoo-item">
+          <img src="/img/tattoo-04-small.jpg" />
+            <h4>ลายไฟ</h4>
+        </div>
         
-      </div>
+        
+        </div> 
     </div>
   );
 }
